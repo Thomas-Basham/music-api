13fb9faa0c2e2cddec73e9923a265d4531c3819e
Thomas Basham <Bashamtg@gmail.com>
2022-06-01 12:22:54 -0700

    updated .env

--- music_api_project/settings.py
+++ music_api_project/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 import dotenv
 from dotenv import load_dotenv
 
-# load_dotenv()  # take environment variables from .env.
+load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_#2c&x1nd%(9z&^36av+nvt*yl(e%t35*q28b_l$1$8b$9t=+'
-# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
