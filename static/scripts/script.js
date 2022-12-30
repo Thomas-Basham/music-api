f8743dc8be8d022cd691d1d650b6ac7f216e692a
Thomas Basham <Bashamtg@gmail.com>
2022-12-29 16:07:13 -0800

    play button and sound waves rendering

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,27 +1,60 @@
-var audio = {
-    init: function() {
-    var $that = this;
-        $(function() {
-            $that.components.media();
-        });
-    },
-    components: {
-        media: function(target) {
-            var media = $('audio.fc-media', (target !== undefined) ? target : 'body');
-            if (media.length) {
-                media.mediaelementplayer({
-                    audioHeight: 40,
-                    features : ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
-                    alwaysShowControls      : true,
-                    timeAndDurationSeparator: '<span></span>',
-                    // iPadUseNativeControls: true,
-                    // iPhoneUseNativeControls: true,
-                    // AndroidUseNativeControls: true
-                });
-            }
-        },
+// let audio = {
+//     init: function () {
+//         let $that = this;
+//         $(function () {
+//             $that.components.media();
+//         });
+//     },
+//     components: {
+//         media: function (target) {
+//             let media = $('audio.fc-media', (target !== undefined) ? target : 'body');
+//             if (media.length) {
+//                 media.mediaelementplayer({
+//                     audioHeight: 40,
+//                     features: ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
+//                     alwaysShowControls: true,
+//                     timeAndDurationSeparator: '<span></span>',
+//                     // iPadUseNativeControls: true,
+//                     // iPhoneUseNativeControls: true,
+//                     // AndroidUseNativeControls: true
+//                 });
+//             }
+//         },
+//
+//     },
+// };
+//
+// audio.init();
+let wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple'
 
-    },
-};
+});
+let source = document.getElementById('audioSource').src;
+wavesurfer.load(source);
+console.log(wavesurfer, wavesurfer.isPlaying())
+if (!wavesurfer.isPlaying()) {
+    $('.play-btn').show();
+    $('.pause-btn').hide();
+} else {
+    $('.pause-btn').show();
+    $('.play-btn').hide();
+}
 
-audio.init();
+function playPause() {
+
+    if (wavesurfer.isPlaying()) {
+        wavesurfer.pause();
+        $('.play-btn').show();
+        $('.pause-btn').hide();
+    } else {
+        wavesurfer.play();
+        $('.pause-btn').show();
+        $('.play-btn').hide();
+    }
+}
+
+
+
+
